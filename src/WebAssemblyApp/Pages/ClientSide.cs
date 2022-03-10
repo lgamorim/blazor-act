efb06c859211f36f36064a3b9ba2c01583adb56b
Luís Amorim <lgamorim@users.noreply.github.com>
2022-03-10 15:40:47 +0000

    Move the code block in the page to a code-behind class

---
+++ src/WebAssemblyApp/Pages/ClientSide.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAct.WebAssemblyApp.Pages;
+
+public partial class ClientSide
+{
+    private IList<Tuple<string, string>> clientSideFeatures = new List<Tuple<string, string>>();
+
+    [Inject]
+    private HttpClient Http { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        clientSideFeatures = await Http.GetFromJsonAsync<IList<Tuple<string, string>>>("sample-data/ClientSide.json");
+    }
+}
