e7ee25466a41074af6da39b8339879527878d349
Luis Amorim <luis.amorim@newday.co.uk>
2022-03-10 17:12:11 +0000

    Created a bare-bones version of a client-side feature detail page.

---
+++ src/WebAssemblyApp/Pages/ClientSideFeature.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAct.WebAssemblyApp.Pages;
+
+public partial class ClientSideFeature
+{
+    private IList<Tuple<string, string>> clientSideFeatures;
+
+    [Inject]
+    private HttpClient Http { get; set; }
+
+    [Parameter]
+    public int Index { get; set; }
+    
+    protected override async Task OnInitializedAsync()
+    {
+        clientSideFeatures = await Http.GetFromJsonAsync<IList<Tuple<string, string>>>("sample-data/ClientSide.json");
+    }
+
+    private Tuple<string, string> GetFeature(int index)
+    {
+        return clientSideFeatures?[index];
+    }
+}
