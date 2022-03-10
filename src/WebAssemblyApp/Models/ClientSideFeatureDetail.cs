4341f41fccdf4bbe1ace91ceb63d29027acf5fcf
Luis Amorim <luis.amorim@newday.co.uk>
2022-03-10 21:31:29 +0000

    Implemented the page that shows the details of a client-side C# feature.

---
+++ src/WebAssemblyApp/Models/ClientSideFeatureDetail.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorAct.WebAssemblyApp.Models;
+
+public class ClientSideFeatureDetail
+{
+    public string Feature { get; set; }
+    public string Description { get; set; }
+    public IEnumerable<string> Details { get; set; } = new List<string>();
+}
