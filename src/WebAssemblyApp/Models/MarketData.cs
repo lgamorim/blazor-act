b1f6dc89cba883ceee20807f9c4f44781e27a9d7
Luis Amorim <luis.amorim@newday.co.uk>
2022-03-17 00:01:28 +0000

    Implemented the new Interop Playground tab that shows an actual live JS/.NET interop call.

---
+++ src/WebAssemblyApp/Models/MarketData.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorAct.WebAssemblyApp.Models;
+
+public class MarketData
+{
+    public IList<IList<double>> Prices { get; set; }
+    
+    [JsonPropertyName("market_caps")]
+    public IList<IList<double>> MarketCaps { get; set; }
+    
+    [JsonPropertyName("total_volumes")]
+    public IList<IList<double>> TotalVolumes { get; set; }
+}
---
+++ src/WebAssemblyApp/wwwroot/js/interop.js
@@ -0,0 +1,37 @@
+﻿let chart;
+window.displayChart = (canvas, prices, days) => {
+    if(chart) {
+        chart.destroy();
+    }
+
+    let context = canvas.getContext('2d');
+    chart = new Chart(context, {
+        type: 'line',
+        data: {
+            labels: days,
+            datasets: [{
+                label: 'Price',
+                data: prices,
+                backgroundColor: 'rgba(0, 102, 204, 0.85)',
+                borderColor: 'rgba(0, 102, 204, 0.85)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
+            }
+        }
+    });
+}
+
+window.callCoinGeckoApi = (dotNetRef) => {
+    let coin = document.getElementById("fc-coin").value;
+    let currency = document.getElementById("fc-currency").value;
+
+    return dotNetRef.invokeMethodAsync("GetMarketData", coin, currency)
+}
